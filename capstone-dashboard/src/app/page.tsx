cb52b2db9a116c1302b99468e69f5827fe4fdcb8
dominic-isaac-molina <dim@hawaii.edu>
2026-04-07 10:02:48 -1000

    Reorganized the side bar

--- capstone-dashboard/src/app/page.tsx
+++ capstone-dashboard/src/app/page.tsx
@@ -1,14 +1,11 @@
 'use client';
 import EcosystemDashboard from "@/components/dashboard/EcosystemDashboard";
-import CommFisheriesDashboard from "@/components/dashboard/commFisheriesMap";
 
 export default function Home() {
   return (
-    
     <EcosystemDashboard
       geoJsonPath="/fisheriesdata/marinecounties.geojson"
       datasetLabel="Fisheries"
     />
-    /*<CommFisheriesDashboard />*/
   );
 }
